9894f3a0e093a89ede48b0fabf69373a91de09d1
m7v-r <ma5766223@gmail.com>
2026-05-23 14:22:36 +0000

    fix: enforce exact argument count for setenv and unsetenv to match checker requirements

--- builtins.c
+++ builtins.c
@@ -27,20 +27,14 @@ int _setenv(const char *variable, const char *value)
 	int i = 0, len;
 	char *new_var;
 
-	if (!variable || _strlen(variable) == 0)
+	if (!variable || !value || _strlen(variable) == 0)
 		return (-1);
 
-	/* If value is NULL, treat it as an empty string "" */
-	if (!value)
-		value = "";
-
-	/* Calculate space needed for VARIABLE=VALUE\0 */
 	len = _strlen(variable) + _strlen(value) + 2;
 	new_var = malloc(len);
 	if (!new_var)
 		return (-1);
 
-	/* Construct the string "VARIABLE=VALUE" */
 	i = 0;
 	while (*variable)
 		new_var[i++] = *variable++;
@@ -49,20 +43,18 @@ int _setenv(const char *variable, const char *value)
 		new_var[i++] = *value++;
 	new_var[i] = '\0';
 
-	/* Check if variable already exists */
 	i = 0;
 	while (environ[i])
 	{
-		if (_strncmp(environ[i], new_var, _strlen(variable)) == 0 &&
-			environ[i][_strlen(variable)] == '=')
+		if (_strncmp(environ[i], new_var, _strlen(new_var) - _strlen(value) - 1) == 0
+			&& environ[i][_strlen(new_var) - _strlen(value) - 1] == '=')
 		{
-			environ[i] = new_var; /* Overwrite existing value */
+			environ[i] = new_var;
 			return (0);
 		}
 		i++;
 	}
 
-	/* If it doesn't exist, append it to environment array */
 	environ[i] = new_var;
 	environ[i + 1] = NULL;
 	return (0);
@@ -76,8 +68,7 @@ int _setenv(const char *variable, const char *value)
  */
 int _unsetenv(const char *variable)
 {
-	int i = 0, j;
-	int len;
+	int i = 0, j, len;
 
 	if (!variable || _strlen(variable) == 0)
 		return (-1);
@@ -85,10 +76,8 @@ int _unsetenv(const char *variable)
 	len = _strlen(variable);
 	while (environ[i])
 	{
-		/* Look for matching variable name */
 		if (_strncmp(environ[i], variable, len) == 0 && environ[i][len] == '=')
 		{
-			/* Shift all elements upwards to close the gap */
 			j = i;
 			while (environ[j])
 			{
@@ -144,27 +133,24 @@ int check_builtins(char **args, int status, char *shell_name, char *line)
 	}
 	if (_strcmp(args[0], "setenv") == 0)
 	{
-		/* Case: setenv without parameters -> Display environment */
-		if (!args[1])
+		if (!args[1] || !args[2])
 		{
-			print_env();
-			return (0);
+			write(STDERR_FILENO, "setenv: Invalid syntax\n", 23);
+			return (1); /* Return failure status */
 		}
-		/* Case: setenv with only one parameter -> value is empty */
 		if (_setenv(args[1], args[2]) == -1)
-			write(STDERR_FILENO, "setenv: Failed to set variable\n", 31);
+			write(STDERR_FILENO, "setenv: Failed\n", 15);
 		return (0);
 	}
 	if (_strcmp(args[0], "unsetenv") == 0)
 	{
-		/* Case: unsetenv without parameters -> Display environment */
 		if (!args[1])
 		{
-			print_env();
-			return (0);
+			write(STDERR_FILENO, "unsetenv: Invalid syntax\n", 25);
+			return (1);
 		}
 		if (_unsetenv(args[1]) == -1)
-			write(STDERR_FILENO, "unsetenv: Failed to unset variable\n", 35);
+			write(STDERR_FILENO, "unsetenv: Failed\n", 17);
 		return (0);
 	}
 	return (-1);
